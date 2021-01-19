d42f338cd633767d1908db5cc08058b1b1ba8376
razorext2 <razorext2@gmail.com>
2021-01-19 01:33:52 +0000

    drop inherit blabla i thought its needed, but no.

--- cherish_surya.mk
+++ cherish_surya.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Device set
 $(call inherit-product, device/xiaomi/surya/device.mk)
